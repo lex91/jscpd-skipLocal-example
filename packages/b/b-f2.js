0167a469a3bb0361dcfe756e86aeb17eed097367
Alexey Krasnyanskiy <akrasnyanskiy@yandex-team.ru>
2021-11-29 20:31:32 +0300

    Init jscpd example

---
+++ packages/b/b-f2.js
@@ -0,0 +1,33 @@
+export const f = () => {
+    let i = 0;
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+    console.log(i++);
+}
